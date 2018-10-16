aed8e55ae3d8a9aae775f84e57b3058b0c0ae072
Joel <joel.vanegmond@ucalgary.ca>
2018-10-16 13:41:35 -0600

    removed debug prints, added documentation

--- authenticate.py
+++ authenticate.py
@@ -1,7 +1,21 @@
+#---------------------------------------
+# Author: Joel van Egmond
+# ID: 10102094
+#
+# Purpose: Checks if the user/password combo entered exists in the database.
+#   If so, prints "Accepted", else prints "Rejected"
+#
+# Usage: python3 authenticate.py [username] [password]
+#
+# Note: requires argon2 library from https://pypi.org/project/argon2/
+#       must be run with python3
+#       user/pass combos are stored in the format [username]:[hashed password]:[salt]
+#---------------------------------------
+
 import sys
 import re
 import os
-import argon2
+import argon2   #from https://pypi.org/project/argon2/
 
 def main(argv):
     if(len(argv) != 3):
@@ -27,38 +41,47 @@ def main(argv):
         if(passwordCorrect(username, password, passTable[username][0], passTable[username][1])):
             print("Accepted")
         else:
-            print("Rejected - password does not match")
+            # Rejected - password does not match
+            print("Rejected")
     else:
-        print("Rejected - username not in file")
+        # Rejected - username not in file
+        print("Rejected")
 
 
-
-#tests if the password is correct
+#---------------------------------------
+# Purpose: Tests if the password is correct by comparing the h(pass,salt) with the stored hash
+# Inputs:
+#   username - name of the user as string
+#   password - user's password as string
+#   storedHashPass - hash of password that is stored in the password file as string
+#   storedSalt - salt corresponding to this user/password hash in the password file
+# Output:
+#   passwordCorrect - whether or not h(pass,salt) matches with the stored hash
+#---------------------------------------
 def passwordCorrect(username, password, storedHashPass, storedSalt):
     hashPass = hashPassword(password, storedSalt.encode(encoding=PWORD_ENCODING))
-    
-    #print("salt")
-    #print(storedSalt.encode(encoding="latin-1"))
-    #print("hashPass")
-    #print(hashPass.decode(encoding="latin-1"))
-    #print("stored hashPass")
-    #print(storedHashPass)
 
     return hashPass.decode(encoding=PWORD_ENCODING) == storedHashPass
 
 
-
+#---------------------------------------
+# Purpose: Uses argon2 to produce a hash from a given password and salt 
+# Inputs:
+#   password - password as a string
+#   salt - a random salt as bytes
+# Output:
+#   argon2 hash - the password and salt hashed in argon2 slow mode (argon2_i), with a size of 32 bytes
+#---------------------------------------
 def hashPassword(password, salt):
     return argon2.argon2_hash(password=password, salt=salt, buflen=HASH_SIZE_IN_BYTES, argon_type=argon2.Argon2Type.Argon2_i)
     
     
-
-# constants and python main thing
-WORDS_FILENAME = "words.txt"    #file containing list of password "words"
-PWORD_FILENAME = "passwords.txt"    #file containing usernames and passwords
-HASH_SIZE_IN_BYTES = 32
-PWORD_ENCODING = "latin-1"
-SEPARATOR_CHAR = ':'
+# Constants
+WORDS_FILENAME = "words.txt"        # file containing list of password "words"
+PWORD_FILENAME = "passwords.txt"    # file containing usernames and passwords
+HASH_SIZE_IN_BYTES = 32             # size of hash in bytes
+PWORD_ENCODING = "latin-1"          # encoding of the password file
+SEPARATOR_CHAR = ':'                # separator character in the password file
 
 if __name__ == "__main__":
     main(sys.argv)
--- enroll.py
+++ enroll.py
@@ -1,7 +1,24 @@
+#---------------------------------------
+# Author: Joel van Egmond
+# ID: 10102094
+#
+# Purpose: Adds the user to the password file if they have a valid (not already used) username,
+#          and a valid password.
+#          A password is invalid if it follows the format [word], [number], [wordnumber], or [numberword]
+#          Where [word] comes from the words.txt file
+#
+# Usage: python3 entroll.py [username] [password]
+#
+# Note: requires argon2 library from https://pypi.org/project/argon2/
+#       must be run with python3
+#       doesn't allow usernames with ':' character in them (need a delimiter)
+#       user/pass combos are stored in the format [username]:[hashed password]:[salt]
+#---------------------------------------
+
 import sys
 import re
 import os
-import argon2
+import argon2   #from https://pypi.org/project/argon2/
 
 def main(argv):
     if(len(argv) != 3):
@@ -15,14 +32,22 @@ def main(argv):
         addUser(username,password)
         print("Accepted")
     
-    
 
-#checks if a username is valid
+#---------------------------------------
+# Purpose: Checks if a username is valid.
+#          A username is valid if it isn't already used, and it doesn't contain
+#          a ':' character (used as a separator in the password file)
+# Input:
+#   username - the username entered by the user as a string
+# Output:
+#   whether or not the username is valid. true if so, false if not
+#---------------------------------------
 def usernameValid(username):
     valid = True
 
     if(SEPARATOR_CHAR in username):
-        print("Rejected - '" + SEPARATOR_CHAR + "' in username")
+        # Rejected - separator ':' in username
+        print("Rejected")
         valid = False
     else:
         usernameList = set({})
@@ -35,89 +60,90 @@ def usernameValid(username):
             passFile = splitPass[1][(HASH_SIZE_IN_BYTES*2)+2:]
             usernameList.add(currentUsername)
 
-        #print(usernameList)
         if(username in usernameList):
-            print("Rejected - username already in use")
+            # Rejected - username already in use
+            print("Rejected")
             valid = False
 
     return valid
 
 
-
-# checks if username is valid
-# no longer used, SUCKS
-def usernameValid_old(username):
-    valid = True
-
-    if(':' in username):
-        print("Rejected - '" + SEPARATOR_CHAR + "' in username")
-        valid = False
-    else:
-        usernameList = set(line.split(':')[0] for line in open(PWORD_FILENAME))
-        print(usernameList)
-        if(username in usernameList):
-            print("Rejected - username already in use")
-            valid = False
-
-    return valid
-
-
-
-#checks if password is valid
+#---------------------------------------
+# Purpose: Checks if the password entered by the user is a valid.
+#          A password is invalid if it follows the format [word], [number], [wordnumber], or [numberword]
+#          Where [word] comes from the words.txt file
+# Input:
+#   password - the password entered by the user, as a string
+# Output:
+#   whether or not the password is valid. true if so, false if not
+#---------------------------------------
 def passwordValid(password):
     valid = True
 
     wordList = set(open(WORDS_FILENAME).read().split())
 
     if(password.isdigit()):
-        print("Rejected - password is [num]")
+        # Rejected - password is [num]
+        print("Rejected")
         valid = False
     elif(password in wordList):
-        print("Rejected - password is [word]")
+        # Rejected - password is [word]
+        print("Rejected")
         valid = False
     else:
         splitPassword = list(filter(None, re.split('(\d+)', password)))
-        # print(splitPassword)
         if(len(splitPassword) == 2):
             if((splitPassword[0] in wordList) and (splitPassword[1].isdigit())):
-                print("Rejected - password is [wordnum]")
+                # Rejected - password is [wordnum]
+                print("Rejected")
                 valid = False
             elif((splitPassword[0].isdigit()) and (splitPassword[1] in wordList)):
-                print("Rejected - password is [numword]")
+                # Rejected - password is [numword]
+                print("Rejected")
                 valid = False
 
     return valid
 
 
-
 #adds a valid user to the file
+#---------------------------------------
+# Purpose: Adds a valid user/password combo to the password file.
+#          Password is hashed together with a random salt using argon2,
+#          and only the username, hash, and salt are stored in the password file.
+#          and salt are stored in the password file.
+#          
+# Inputs:
+#   username - the username chosen by the user, as a string
+#   password - the password chosen by the user, as a string
+# Output:
+#   whether or not the password is valid. true if so, false if not
+#---------------------------------------
 def addUser(username, password):
-
     salt = os.urandom(HASH_SIZE_IN_BYTES)
     hashPass = hashPassword(password, salt)
-
-    #print("salt")
-    #print(salt)
-    #print("hashPass")
-    #print(hashPass.decode(encoding="latin-1"))
 
     with open(PWORD_FILENAME, 'a', encoding=PWORD_ENCODING) as pwordFile:
         pwordFile.write(username + SEPARATOR_CHAR + hashPass.decode(encoding=PWORD_ENCODING) + SEPARATOR_CHAR + salt.decode(encoding=PWORD_ENCODING) + '\n') # could be any text, appended @ the end of file
 
 
-#hashes the password
+#---------------------------------------
+# Purpose: Uses argon2 to produce a hash from a given password and salt 
+# Inputs:
+#   password - password as a string
+#   salt - a random salt as bytes
+# Output:
+#   argon2 hash - the password and salt hashed in argon2 slow mode (argon2_i), with a size of 32 bytes
+#---------------------------------------
 def hashPassword(password, salt):
     return argon2.argon2_hash(password=password, salt=salt, buflen=HASH_SIZE_IN_BYTES, argon_type=argon2.Argon2Type.Argon2_i)
     
     
-    
-# constants and python main thing
-
-WORDS_FILENAME = "words.txt"    #file containing list of password "words"
-PWORD_FILENAME = "passwords.txt"    #file containing usernames and passwords
-HASH_SIZE_IN_BYTES = 32
-PWORD_ENCODING = "latin-1"
-SEPARATOR_CHAR = ':'
+# Constants
+WORDS_FILENAME = "words.txt"        # file containing list of password "words"
+PWORD_FILENAME = "passwords.txt"    # file containing usernames and passwords
+HASH_SIZE_IN_BYTES = 32             # size of hash in bytes
+PWORD_ENCODING = "latin-1"          # encoding of the password file
+SEPARATOR_CHAR = ':'                # separator character in the password file
 
 if __name__ == "__main__":
     main(sys.argv)
